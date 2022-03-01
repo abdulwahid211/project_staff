fb8dffe8fbb09bd1ffd8d7df3a99573fe289e235
Abdul Wahid <abdulwahid211@gmail.com>
2022-03-01 12:36:50 +0000

    upload foundation

---
+++ db/sql/upload_tables.sql
@@ -0,0 +1,30 @@
+
+CREATE TABLE Applicants (
+    ApplicantID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+CREATE TABLE Admin (
+    AdminID int,
+    LastName varchar(255),
+    FirstName varchar(255)
+);
+
+CREATE TABLE Vacancies (
+    VacancyID int,
+    Title varchar(255)
+);
+
+CREATE TABLE Companies (
+    CompanyID int,
+    Name varchar(255)
+);
+
+CREATE TABLE AppliedJobs (
+    AppliedJobsID int,
+    ApplicantID int,
+    VacancyID int
+);
