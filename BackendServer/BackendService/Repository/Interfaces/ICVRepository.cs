931ee3531c72ffd29d7fc126475b017b83e59f9e
Abdul <abdulwahid211@gmail.com>
2023-08-26 03:23:14 +0100

    Create ICVRepository.cs

---
+++ BackendServer/BackendService/Repository/Interfaces/ICVRepository.cs
@@ -0,0 +1,11 @@
+﻿using BackendService.Model;
+
+namespace BackendService.Repository.Interfaces
+{
+    public interface ICVRepository
+    {
+        public Task<string> DownloadCV(IHttpContextAccessor http);
+        public Task<bool> DeleteCV(string email, IHttpContextAccessor http);
+        public Task<bool> UploadCV(CV file, IHttpContextAccessor http);
+    }
+}
